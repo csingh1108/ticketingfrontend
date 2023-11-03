a82ccbf35868b575780c5137f999401da692102c
csingh1108 <113258536+csingh1108@users.noreply.github.com>
2023-11-03 10:20:32 -0700

    exploring creating a live chat.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import TicketListView from "./TicketListView";
+import TicketDetails from "./TicketDetailsView";
+import Comments from "./Comments";
+import TicketDashboardView from "./TicketDashboardView";
+import TicketDashboard from "./TicketDashboardView";
+import Chat from "./Chat";
+import SupportWindow from "./SupportWindow";
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+        <Route path="" element={ <TicketListView/>}/>
+        <Route path="/tickets/main" element={ <TicketListView/>}/>
+        <Route path="/ticket/:ticketId" element={ <TicketDetails/> }/>
+        <Route path="/dashboard" element={ <TicketDashboard/>} />
+        <Route path="/support" element={ <SupportWindow/>} />
+    </Routes>
   );
 }
 
